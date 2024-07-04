9fe87bbe50efcaf502c9ff4087a9b21ddb5a4a22
xcellsx <154584328+xcellsx@users.noreply.github.com>
2024-07-04 17:48:14 +0800

    Add files via upload

---
+++ HCI/notes.php
@@ -0,0 +1,100 @@
+<!-- <html>
+    <head>
+        <title></title>
+    </head>
+
+    <body> -->
+        
+        <!-- // $dbhost = 'localhost';
+        // $dbuser = 'root';
+        // $dbpass = '';
+        // $dbName = 'usertest1';
+
+        // $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbName);
+        // if($mysqli -> connect_errno){
+        //     printf("connection failed", $mysqli -> connect_error);
+        //     exit();
+        // }
+        // printf("connected successfully");
+
+        // To create DB
+        // if ($mysqli -> query("create database usertest1")) {
+        //     printf("Database created successfully<br/>");
+        // }
+
+        // if ($mysqli -> errno) {
+        //     printf("Could not create database<br/>", $mysqli->error);
+        // }
+
+
+        // Select DB
+        // $dbselect = mysqli_select_db($mysqli, 'usertest1');
+
+        // if (!$dbselect) {
+        //     die("Could not select db: " . mysqli_error($mysqli));
+        // }
+
+        // printf( "Database selected sucess");
+
+        // Create Table
+        // $sql = "create table usertest (
+        //         id int, 
+        //         name varchar(128), 
+        //         username varchar(128),
+        //         email varchar(255),
+        //         password varchar(128),
+        //         phone varchar(20),
+        //         birthday DATE,
+        //         bank varchar(255),
+        //         account varchar(20))";
+
+        // if ($mysqli -> query($sql)) {
+        //     printf("Table created success <br/>");
+        // }
+
+        // if ($mysqli -> errno) {
+        //     printf("table fail", $mysqli -> error);
+        // }
+
+        // $id = 1;
+        // $name = "celine";
+        // $username = "cells";
+        // $email = "cel@gmail.com";
+        // $password = "123";
+        // $phone = "123";
+        // $birthday = "24-Jan-1990";
+        // $bank = "posb";
+        // $account = "123";
+        // $sql = "insert into usertest (
+        //         id, 
+        //         name, 
+        //         username, 
+        //         email, 
+        //         password, 
+        //         phone, 
+        //         birthday, 
+        //         bank, 
+        //         account
+        //         ) values (
+        //             ".$id.", 
+        //             '".$name."',
+        //             '".$username."', 
+        //             '".$email."', 
+        //             '".$password."', 
+        //             '".$phone."', 
+        //             '".$birthday."', 
+        //             '".$bank."', 
+        //             '".$account."'
+        //         )";
+        
+        // if ($mysqli-> query($sql)) {
+        //     printf("data inserted <br/>");
+        // }
+
+        // if ($mysqli -> errno) {
+        //     printf("error", $mysqli->error);
+        // }
+
+        // $mysqli->close();
+// </body>
+// </html> -->
